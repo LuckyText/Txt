80e5a3eb30a94f566f01cf19e231228374cda6ca
LuckyText <109129926+LuckyText@users.noreply.github.com>
2023-03-22 14:16:23 +0800

    Update action.sh

--- action.sh
+++ action.sh
@@ -6,16 +6,16 @@ rm 3
 rm 4
 
 time=$(date -d "-1 days" "+%m%d")
-echo "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/2023@main/${time}.txt")" > 1
-echo "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/2023@main/${time}clash.yml")" > 2.txt
-echo "$(curl "https://sub.pmsub.me/clash.yaml")" > 3.txt
-echo "$(curl "https://sub.pmsub.me/base64")" > 4
-
+curl -sL --fail "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/2023@main/${time}.txt" -o 1
+if curl -sL --fail "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/2023@main/${time}clash.yml" -o 2.txt; then
 sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 2.txt > 2
-sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 3.txt > 3
-
 rm 2.txt
+fi
+if curl -sL --fail "https://sub.pmsub.me/clash.yaml" -o 3.txt; then
+sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 3.txt > 3
 rm 3.txt
+fi
+curl -sL --fail "https://sub.pmsub.me/base64" -o 4
 
 git config --global user.name '111'
 git config --global user.email '345@ak47.com'
