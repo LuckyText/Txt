eb62398be9611e3dc700fa431fd8691876789f33
LuckyText <109129926+LuckyText@users.noreply.github.com>
2023-11-19 23:43:31 -0600

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,34 +2,37 @@
 
 export LANG=C.UTF-8
 
-time=$(date -d "-1 days" "+%m%d")
-year=$(date -d "-1 days" "+%Y")
-month=$(date -d "-1 days" "+%m")
-day=$(date -d "-1 days" "+%d")
+run() {
+    year=$(date -d "$1 days" "+%Y")
+    month=$(date -d "$1 days" "+%m")
+    day=$(date -d "$1 days" "+%d")
+    if curl -sL --fail "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/${year}@main/${month}${day}.txt" -o 1.txt; then
+    cp 1.txt 1
+    fi
+    
+    if curl -sL --fail "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/${year}@main/${month}${day}clash.yml" -o 2.txt; then
+    sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 2.txt > 2
+    fi
+    
+    if curl -sL --fail "https://nodefree.org/dy/${year}/${month}/${year}${month}${day}.yaml" -o 3.txt; then
+    sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 3.txt > 3
+    fi
+    
+    if curl -sL --fail "https://nodefree.org/dy/${year}/${month}/${year}${month}${day}.txt" -o 4.txt; then
+    cp 4.txt 4
+    fi
+    
+    if curl -sL --fail "https://freenode.openrunner.net/uploads/${year}${month}${day}-clash.yaml" -o 5.txt; then
+    sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 5.txt > 5
+    fi
+    
+    if curl -sL --fail "https://freenode.openrunner.net/uploads/${year}${month}${day}-v2ray.txt" -o 6.txt; then
+    cp 6.txt 6
+    fi
+}
 
-if curl -sL --fail "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/2023@main/${time}.txt" -o 1.txt; then
-cp 1.txt 1
-fi
-
-if curl -sL --fail "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/2023@main/${time}clash.yml" -o 2.txt; then
-sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 2.txt > 2
-fi
-
-if curl -sL --fail "https://nodefree.org/dy/${year}/${month}/${year}${month}${day}.yaml" -o 3.txt; then
-sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 3.txt > 3
-fi
-
-if curl -sL --fail "https://nodefree.org/dy/${year}/${month}/${year}${month}${day}.txt" -o 4.txt; then
-cp 4.txt 4
-fi
-
-if curl -X POST -H "Content-Type: application/x-www-form-urlencoded; charset=utf-8" -sL --fail "https://freenode.openrunner.net/uploads/${year}${month}${day}-clash.yaml" -o 5.txt; then
-sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 5.txt > 5
-fi
-
-if curl -sL --fail "https://freenode.openrunner.net/uploads/${year}${month}${day}-v2ray.txt" -o 6.txt; then
-cp 6.txt 6
-fi
+run "-1"
+run "0"
 
 rm 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt
 
