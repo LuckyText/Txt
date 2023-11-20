4fb74eb03cba2acc9ddf7e80cd478893414463a0
LuckyText <109129926+LuckyText@users.noreply.github.com>
2023-11-19 23:24:30 -0600

    Update action.sh

--- action.sh
+++ action.sh
@@ -3,6 +3,10 @@
 export LANG=C.UTF-8
 
 time=$(date -d "-1 days" "+%m%d")
+year=$(date -d "-1 days" "+%Y")
+month=$(date -d "-1 days" "+%m")
+day=$(date -d "-1 days" "+%d")
+
 if curl -sL --fail "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/2023@main/${time}.txt" -o 1.txt; then
 cp 1.txt 1
 fi
@@ -11,15 +15,14 @@ if curl -sL --fail "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/2023@main/${time
 sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 2.txt > 2
 fi
 
-if curl -sL --fail "https://sub.pmsub.me/clash.yaml" -o 3.txt; then
+if curl -sL --fail "https://nodefree.org/dy/${year}/${month}/${year}${month}${day}.yaml" -o 3.txt; then
 sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 3.txt > 3
 fi
 
-if curl -sL --fail "https://sub.pmsub.me/base64" -o 4.txt; then
+if curl -sL --fail "https://nodefree.org/dy/${year}/${month}/${year}${month}${day}.txt" -o 4.txt; then
 cp 4.txt 4
 fi
 
-time1=$(date -d "-1 days" "+%Y%m%d")
 if curl -sL --fail "https://freenode.openrunner.net/uploads/${time1}-clash.yaml" -o 5.txt; then
 sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 5.txt > 5
 fi
