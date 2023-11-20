cb149cd181f02bb8d427369d27a843452bb9f7ff
LuckyText <109129926+LuckyText@users.noreply.github.com>
2023-11-19 22:10:12 -0600

    Update action.sh

--- action.sh
+++ action.sh
@@ -17,7 +17,16 @@ if curl -sL --fail "https://sub.pmsub.me/base64" -o 4.txt; then
 cp 4.txt 4
 fi
 
-rm 1.txt 2.txt 3.txt 4.txt
+time1=$(date -d "-1 days" "+%Y%m%d")
+if curl -sL --fail "https://freenode.openrunner.net/uploads/${time1}-clash.yaml" -o 5.txt; then
+sed -e '/ - DOMAIN-SUFFIX,admob.com,🛑 全球拦截/d' 5.txt > 5
+fi
+
+if curl -sL --fail "https://freenode.openrunner.net/uploads/${time1}-v2ray.txt" -o 6.txt; then
+cp 6.txt 6
+fi
+
+rm 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt
 
 git config --global user.name '111'
 git config --global user.email '345@ak47.com'
