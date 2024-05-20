bb01230a5597849c5fb0dc248127c08bf9e45d49
LuckyText <109129926+LuckyText@users.noreply.github.com>
2024-05-20 15:20:39 +0800

    Update action.sh

--- action.sh
+++ action.sh
@@ -23,7 +23,8 @@ run() {
     fi
     
     if curl -sL --fail "https://freenode.openrunner.net/uploads/${year}${month}${day}-clash.yaml" -o 5.txt; then
-    sed -e '/  - DOMAIN-SUFFIX,admob.com,全球拦截/d' 5.txt > 5
+    sed -e '/  - DOMAIN-SUFFIX,admob.com,全球拦截/d' 5.txt > 5.txt
+    sed -e '/  - DOMAIN-SUFFIX,admob.com,🍃 应用净化/d' 5.txt > 5
     fi
     
     if curl -sL --fail "https://freenode.openrunner.net/uploads/${year}${month}${day}-v2ray.txt" -o 6.txt; then
